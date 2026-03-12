dfe5f43360c8a058c6945153e82bed16ee0db388
PATRICIA PAEZ CARDENAS <patriciapaezcardenas@gmail.com>
2026-03-12 05:56:07 -0500

    Primer commit

---
+++ src/AboutmeComponent.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function AboutmeComponent() {
+  return (
+    <div>
+      <h1>Patricia Páez Cárdenas</h1>
+      <p>
+        Desarrolladora web con experiencia en desarrollo backend con C# y en
+        proceso de especialización en desarrollo frontend con JavaScript, HTML,
+        CSS y React. Me enfoco en construir soluciones que reflejen
+        correctamente la lógica del negocio, priorizando la calidad del código,
+        las buenas prácticas y una experiencia de usuario clara y funcional.
+      </p>
+      <p>
+        <b> Proyecto 1</b>{" "}
+      </p>
+      <p>Título:</p>
+      <p>"Web Project Around Es"</p>
+      <p>Problema:</p>
+      Se requiere recopilación de los lugares por los cuales he viajado.
+      <p>Qué hice:</p>
+      Desarrollo de página web para ingresar una tarjeta por cada lugar
+      visitado.
+      <p>Resultado o aprendizaje:</p>
+      <img src="../public/Proyecto_1_Evidencia.png" alt="Imagen proyecto"></img>
+      Link para repositorio
+    </div>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import AboutmeComponent from './AboutmeComponent';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+       
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,9 +18,12 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>        
       </header>
-    </div>
+      <body>
+         <AboutmeComponent></AboutmeComponent>
+      </body>
+    </div>    
   );
 }
 
